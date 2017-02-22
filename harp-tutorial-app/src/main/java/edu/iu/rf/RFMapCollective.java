8260b80a948eff796b1c83a22047904afd4eb076
imingtsou <yizou@iu.edu>
2017-02-22 17:19:59 -0500

    rf launcher (TBC)

--- harp-tutorial-app/src/main/java/edu/iu/rf/RFMapCollective.java
+++ harp-tutorial-app/src/main/java/edu/iu/rf/RFMapCollective.java
@@ -33,7 +33,43 @@ public class RFMapCollective extends Configured implements Tool {
             ToolRunner.printGenericCommandUsage(System.err);
             return -1;
         }
+
+        //arguments assign
+
+        Configuration configuration = this.getConf();
+        FileSystem fs = FileSystem.get((Configuration)configuration);
+
+        Path outDirPath = new Path(outputPath);
+        if (fs.exists(outDirPath)) {
+            fs.delete(outDirPath, true);
+        }
+
+        Job job = configureRFJob();
+        boolean jobSuccess = job.waitForCompletion(true);
+        if (!jobSuccess) {
+            System.out.println("Random Forests job fails.");
+        }
+        return 0;
     }
 
+    private Job configureRFJob() throws IOException, URISyntaxException {
+        Job job = Job.getInstance(configuration, "RF_job");
+        JobConf jobConf = (JobConf)job.getConfiguration();
+        Configuration jobConfiguration - job.getConfiguration();
 
+        FileInputFormat.setInputPaths(job, new Path(dataPath));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+        job.setInputFormatClass(MultiFileInputFormat.class);
+        job.setJarByClass(RFMapCollective.class);
+        job.setMapperClass(RFMapper.class);
+
+        jobConf.set("mapreduce.framework.name", "map-collective");
+        jobConf.setNumMapTasks(numMapTasks);
+        jobConf.setInt("mapreduce.job.max.split.locations", 10000);
+        job.setNumReduceTasks(0);
+
+        //jobConfiguration constants assign
+
+        return job;
+    }
 }
