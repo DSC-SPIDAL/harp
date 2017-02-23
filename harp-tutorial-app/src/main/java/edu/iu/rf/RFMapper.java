2335d14ad8396d83255b67d7e22c8bfb02b901f4
imingtsou <imingtsou@gmail.com>
2017-02-23 10:08:27 -0500

    test ssh key

--- harp-tutorial-app/src/main/java/edu/iu/rf/RFMapper.java
+++ harp-tutorial-app/src/main/java/edu/iu/rf/RFMapper.java
@@ -1,1 +1,14 @@
 package edu.iu.rf;
+
+import java.util.*;
+import java.io.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.CollectiveMapper;
+
+import edu.iu.harp.example.DoubleArrPlus;
+import edu.iu.harp.partition.Partition;
+import edu.iu.harp.partition.Table;
+import edu.iu.harp.resource.DoubleArray;
+import edu.iu.harp.partition.Partitioner;
+import edu.iu.harp.schdynamic.DynamicScheduler;
+
