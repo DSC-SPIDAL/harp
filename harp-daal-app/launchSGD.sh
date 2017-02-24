0105258f39898cd8a997adff06a372d2431e95a3
chenlangshi <chenlangshi@gmail.com>
2017-02-23 23:24:58 -0500

    correct the typo of harp3

--- harp-daal-app/launchSGD.sh
+++ harp-daal-app/launchSGD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp build/harp3-daal-app-hadoop-2.6.0.jar ${HADOOP_HOME}/
+cp build/harp-daal-app-hadoop-2.6.0.jar ${HADOOP_HOME}/
 
 export LIBJARS=${DAALROOT}/lib/daal.jar
 export Time=$(date +%y-%m-%d-%H:%M:%S)
@@ -37,17 +37,17 @@ cd ${HADOOP_HOME}
 
 # ---------------------- test Harp-DAAL ----------------------
 
-# bin/hadoop jar harp3-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /Hadoop/sgd-input/netflix-train 40 0.05 0.002 10 2 40 0 2 false /Hadoop/sgd-work /Hadoop/sgd-input/netflix-test  
+# bin/hadoop jar harp-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /Hadoop/sgd-input/netflix-train 40 0.05 0.002 10 2 40 0 2 false /Hadoop/sgd-work /Hadoop/sgd-input/netflix-test  
 
-# bin/hadoop jar harp3-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /Hadoop/sgd-input/movielens-train 128 0.003 0.005 5 2 40 0 2 false /Hadoop/sgd-work /Hadoop/sgd-input/movielens-test  
+# bin/hadoop jar harp-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /Hadoop/sgd-input/movielens-train 128 0.003 0.005 5 2 40 0 2 false /Hadoop/sgd-work /Hadoop/sgd-input/movielens-test  
 
-bin/hadoop jar harp3-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /Hadoop/sgd-input/yahoomusic-train 128 1 0.0001 5 2 40 0 2 false /Hadoop/sgd-work /Hadoop/sgd-input/yahoomusic-test  
+bin/hadoop jar harp-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /Hadoop/sgd-input/yahoomusic-train 128 1 0.0001 5 2 40 0 2 false /Hadoop/sgd-work /Hadoop/sgd-input/yahoomusic-test  
 
-# bin/hadoop jar harp3-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /f-hugewiki-train-II 128 0.01 0.004 10 30 30 0 2 false /Hadoop/sgd-work /f-hugewiki-test-II  
+# bin/hadoop jar harp-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /f-hugewiki-train-II 128 0.01 0.004 10 30 30 0 2 false /Hadoop/sgd-work /f-hugewiki-test-II  
 
-# bin/hadoop jar harp3-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /f-hugewiki-train 128 0.01 0.0008 10 30 30 0 2 false /Hadoop/sgd-work /f-hugewiki-test  
+# bin/hadoop jar harp-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /f-hugewiki-train 128 0.01 0.0008 10 30 30 0 2 false /Hadoop/sgd-work /f-hugewiki-test  
 
-# bin/hadoop jar harp3-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /hugewiki-train 128 0.01 0.004 5 30 30 0 2 false /Hadoop/sgd-work /hugewiki-test  
+# bin/hadoop jar harp-daal-app-hadoop-2.6.0.jar edu.iu.daal_sgd.SGDDaalLauncher -libjars ${LIBJARS} /hugewiki-train 128 0.01 0.004 5 30 30 0 2 false /Hadoop/sgd-work /hugewiki-test  
 
 
 
