349432d317891180f751d55d5c4bf0bff064cd04
imingtsou <imingtsou@gmail.com>
2017-03-03 13:54:56 -0500

    change to Dataset class

--- harp-tutorial-app/src/main/java/edu/iu/rf/Util.java
+++ harp-tutorial-app/src/main/java/edu/iu/rf/Util.java
@@ -10,6 +10,11 @@ import edu.iu.harp.partition.Partition;
 import edu.iu.harp.partition.Table;
 import edu.iu.harp.resource.DoubleArray;
 
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.DenseInstance;
+import net.sf.javaml.core.Instance;
+
 public class Util {
 	public static void loadData(String pathString, ArrayList<Sample> data, Configuration configuration) throws IOException {
 		Path path = new Path(pathString);
@@ -27,6 +32,28 @@ public class Util {
 					features.add(Float.parseFloat(numbers[i]));
 				}
 				data.add(new Sample(label, features));
+			}
+		}
+		finally {
+			in.close();
+		}
+	}
+
+	public static void loadDataset(String pathString, Dataset dataset, Configuration configuration) throws IOException {
+		Path path = new Path(pathString);
+		FileSystem fs = path.getFileSystem(configuration);
+		FSDataInputStream in = fs.open(path);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] numbers = line.split(" ");
+				Double[] features = new Double[numbers.length - 1];
+				for (int i = 0; i < (numbers.length - 1); i++) {
+					features[i] = Double.parseDouble(numbers[i]);
+				}
+				dataset.add(new DenseInstance(features, numbers[numbers.length - 1]));
 			}
 		}
 		finally {
