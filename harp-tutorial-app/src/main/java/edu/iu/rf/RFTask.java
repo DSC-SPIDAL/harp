3d960f8f4ef7005d70841ebecc750bbc6d555b56
imingtsou <imingtsou@gmail.com>
2017-03-06 15:38:03 -0500

    test RFTask

--- harp-tutorial-app/src/main/java/edu/iu/rf/RFTask.java
+++ harp-tutorial-app/src/main/java/edu/iu/rf/RFTask.java
@@ -16,14 +16,14 @@ import net.sf.javaml.classification.tree.RandomForest;
 import net.sf.javaml.classification.tree.RandomTree;
 
 public class RFTask implements Task<Dataset, Classifier> {
-	private int numFeatures;
+    private int numFeatures;
 
-	public RFTask(int numFeatures) {
-		this.numFeatures = numFeatures;
-	}
+    public RFTask(int numFeatures) {
+        this.numFeatures = numFeatures;
+    }
 
-	@Override
-    public Object run(Dataset dataset) throws Exception {
+    @Override
+    public Classifier run(Dataset dataset) throws Exception {
     	Classifier rf = new RandomForest(1, false, numFeatures, new Random());
     	rf.buildClassifier(dataset);
     	return rf;
