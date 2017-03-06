953f04240fbb6cfbcb47817304045e9debe821d9
imingtsou <imingtsou@gmail.com>
2017-03-06 17:10:58 -0500

    fix timeout

--- harp-tutorial-app/src/main/java/edu/iu/rf/RFTask.java
+++ harp-tutorial-app/src/main/java/edu/iu/rf/RFTask.java
@@ -1,6 +1,10 @@
 package edu.iu.rf;
 
 import java.util.*;
+import java.io.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.CollectiveMapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
 import edu.iu.harp.partition.Partition;
 import edu.iu.harp.resource.DoubleArray;
@@ -17,15 +21,18 @@ import net.sf.javaml.classification.tree.RandomTree;
 
 public class RFTask implements Task<Dataset, Classifier> {
     private int numFeatures;
+    private Context context;
 
     public RFTask(int numFeatures) {
         this.numFeatures = numFeatures;
+        this.context = context;
     }
 
     @Override
     public Classifier run(Dataset dataset) throws Exception {
     	Classifier rf = new RandomForest(1, false, numFeatures, new Random());
     	rf.buildClassifier(dataset);
+        context.progress();
     	return rf;
     }
 }
