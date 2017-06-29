3e6ec6f98eff27bba92d171db4baddca4a555b58
lc37 <lc37@indiana.edu>
2017-06-29 14:20:20 -0400

    add daal-src as submodule

--- harp-daal-app/daal-src/algorithms/kernel/_daal_version_data.h
+++
@@ -1,8 +0,0 @@
-// DO NOT EDIT: file is auto-generated on build time
-// DO NOT PUT THIS FILE TO SVN: file is auto-generated on build time
-// Product version is specified in src/makefile.ver file
-#define MAJORVERSION 2017
-#define MINORVERSION 0
-#define UPDATEVERSION 0
-#define BUILD "20170616"
-#define PRODUCT_STATUS 'P'
