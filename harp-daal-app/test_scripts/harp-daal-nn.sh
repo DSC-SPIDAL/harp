f23e34db3ea21f9a73dec9ef544455686b707876
francktcheng <lc37@indiana.edu>
2017-07-23 14:30:13 -0400

    update harp-daal-app example scripts

---
+++ harp-daal-app/test_scripts/harp-daal-nn.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+## export the HARP_DAAL_ROOT
+cd ../
+export HARP_DAAL_ROOT=$(pwd)
+
+if [ -z ${HADOOP_HOME+x} ];then
+    echo "HADOOP not setup"
+    exit
+fi
+
+cp ${HARP_DAAL_ROOT}/target/harp-daal-app-1.0-SNAPSHOT.jar ${HADOOP_HOME}
+
+## check if DAAL env is setup 
+if [ -z ${DAALROOT+x} ];then
+    echo "DAAL not installed, please setup DAALROOT"
+    exit
+else
+    echo "${DAALROOT}"
+fi
+
+cd ${HADOOP_HOME}
+
+hdfs dfsadmin -safemode get | grep -q "ON"
+if [[ "$?" = "0" ]]; then
+    hdfs dfsadmin -safemode leave
+fi
+
+# put daal and tbb, omp libs to hdfs, they will be loaded into the distributed cache
+hdfs dfs -mkdir -p /Hadoop/Libraries
+hdfs dfs -rm /Hadoop/Libraries/*
+hdfs dfs -put ${DAALROOT}/lib/intel64_lin/libJavaAPI.so /Hadoop/Libraries/
+hdfs dfs -put ${DAALROOT}/../tbb/lib/intel64_lin/gcc4.4/libtbb* /Hadoop/Libraries/
+hdfs dfs -put ${HARP_DAAL_ROOT}/external/omp/libiomp5.so /Hadoop/Libraries/
+
+## log directory
+mkdir -p ${HADOOP_HOME}/Harp-DAAL-LOG
+logDir=${HADOOP_HOME}/Harp-DAAL-LOG
+
+export LIBJARS=${DAALROOT}/lib/daal.jar
+
+Dataset=daal_nn
+Mem=110000
+Batch=50
+# num of mappers (nodes)
+Node=2
+# num of threads on each mapper(node)
+Thd=16
+
+echo "Test-daal-nn-$Dataset-N$Node-T$Thd-B$Batch Start" 
+hadoop jar harp-daal-app-1.0-SNAPSHOT.jar edu.iu.daal_nn.NNDaalLauncher -libjars ${LIBJARS}  /Hadoop/nn-input/$Dataset/train /Hadoop/nn-input/$Dataset/test /Hadoop/nn-input/$Dataset/groundTruth /nn/work $Mem $Batch $Node $Thd 2>$logDir/Test-daal-nn-$Dataset-N$Node-T$Thd-B$Batch.log 
+echo "Test-daal-nn-$Dataset-N$Node-T$Thd-B$Batch End" 
