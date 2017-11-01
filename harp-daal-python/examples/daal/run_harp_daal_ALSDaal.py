30a90cfd89a7b2f86c0ad1dc0fafa963b48c865d
Bo Feng <bfeng1216@gmail.com>
2017-11-01 17:30:15 -0400

    changed args of ALS

--- harp-daal-python/examples/daal/run_harp_daal_ALSDaal.py
+++ harp-daal-python/examples/daal/run_harp_daal_ALSDaal.py
@@ -2,6 +2,6 @@ from harp.daal.applications import ALSDaalApplication
 
 my_app = ALSDaalApplication('My ALSDaal with Harp')
 
-my_app.args('/netflix/netflix-train 100 0.05 0.002 4 false 2 16 10240 /als-work /netflix/netflix-test')
+my_app.args('/movielens/movielens-train 100 0.05 0.002 4 false 1 8 10240 /als-work /movielens/movielens-test')
 
 my_app.run()
