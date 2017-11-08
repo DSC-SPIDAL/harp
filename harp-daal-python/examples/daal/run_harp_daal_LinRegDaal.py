6782a230fc3da25692564cd33e29be8b5655c9f2
Bo Feng <bfeng1216@gmail.com>
2017-11-07 21:52:31 -0500

    args changed to fit 14GB VM

--- harp-daal-python/examples/daal/run_harp_daal_LinRegDaal.py
+++ harp-daal-python/examples/daal/run_harp_daal_LinRegDaal.py
@@ -2,6 +2,6 @@ from harp.daal.applications import LinRegDaalApplication
 
 my_app = LinRegDaalApplication('My LinRegDaal with Harp')
 
-my_app.args('/daal_reg/train /daal_reg/test /daal_reg/groundTruth /linreg-work 10240 50 2 16')
+my_app.args('/daal_reg/train /daal_reg/test /daal_reg/groundTruth /linreg-work 6000 50 1 4')
 
 my_app.run()
