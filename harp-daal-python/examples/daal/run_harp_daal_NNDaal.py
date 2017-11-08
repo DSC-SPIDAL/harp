66541a525523ad1a9daa40c07c0908ada4f97df6
Bo Feng <bfeng1216@gmail.com>
2017-11-07 21:52:31 -0500

    args changed to fit 14GB VM

--- harp-daal-python/examples/daal/run_harp_daal_NNDaal.py
+++ harp-daal-python/examples/daal/run_harp_daal_NNDaal.py
@@ -2,6 +2,6 @@ from harp.daal.applications import NNDaalApplication
 
 my_app = NNDaalApplication('My NNDaal with Harp')
 
-my_app.args('/daal_nn/train /daal_nn/test /daal_nn/groundTruth /nn-work 10240 50 2 16')
+my_app.args('/daal_nn/train /daal_nn/test /daal_nn/groundTruth /nn-work 6000 50 1 4')
 
 my_app.run()
