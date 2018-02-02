e22df02b4387c6d9afe5f28b3e7c81a371b8bd2b
Langshi Chen <lc37@140-182-206-31.dhcp-bl.indiana.edu>
2018-02-02 11:34:09 -0500

    remove compile.sh from repo

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## if setup the DAALROOT for experimental apps 
-# export DAALROOT=/N/u/lc37/Lib/DAAL2018_Beta/__release_subgraph_hsw_lnx/daal
-export DAALROOT=/N/u/lc37/IntelDAAL/daal/__release_lnx/daal
-
-mvn clean package
