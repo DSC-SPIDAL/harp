5685632f37978f21e39d413c27eaa5023379a07e
pengb <pengb@indiana.edu>
2018-02-14 10:46:42 -0500

    add test_script for mlr under harp-tutorial-app/tst_scripts

---
+++ harp-tutorial-app/test_scripts/mlr.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#
+# test script for MLR, using the rcv1v2 dataset
+#
+
+#get the startup directory
+startdir=$(dirname $0)
+harproot=$(readlink -m $startdir/../../)
+datadir=$harproot/datasets/tutorial/rcv1
+bin=$harproot/harp-tutorial-app/target/harp-tutorial-app-1.0-SNAPSHOT.jar
+hdfsroot=/harp-test
+hdfsdatadir=$hdfsroot/rcv1/
+hdfsoutput=$hdfsroot/mlr/
+
+echo $startdir,$harproot
+
+
+if [ ! -d $datadir ] ; then
+    echo "rcv1 dataset not found at "$datadir
+    exit -1
+fi
+if [ ! -f $bin ] ; then
+    echo "harp tutorial app not found at "$bin
+    exit -1
+fi
+if [ -z ${HADOOP_HOME+x} ];then
+    echo "HADOOP not setup"
+    exit
+fi
+
+#workdir
+workdir=test_mlr
+
+mkdir -p test_mlr
+cd test_mlr
+
+#check dataset
+runlog=$(hadoop fs -ls $hdfsdatadir/rcv1.topics.txt)
+if [ $? -ne 0 ]; then
+    echo "initialize the dataset on hdfs......"
+
+    hadoop fs -rm -r -f $hdfsdatadir
+    hadoop fs -mkdir -p $hdfsdatadir
+    hadoop fs -put $datadir/rcv1* $hdfsdatadir
+
+    rm -rf data
+    mkdir -p data
+    cd data
+    split -l 1000 $datadir/lyrl2004_vectors_train.dat
+    cd ..
+    hadoop fs -put data $hdfsdatadir
+    echo "done!"
+fi
+
+#run test
+#$ hadoop jar harp-tutorial-app-1.0-SNAPSHOT.jar edu.iu.mlr.MLRMapCollective [alpha] [number of iteration] [number of features] [number of workers] [number of threads] [topic file path] [qrel file path] [input path in HDFS] [output path in HDFS]
+hadoop jar $bin edu.iu.mlr.MLRMapCollective 1.0 5 47236 2 16 $hdfsdatadir/rcv1.topics.txt $hdfsdatadir/rcv1-v2.topics.qrels $hdfsdatadir/data $hdfsoutput
+if [ $? -ne 0 ]; then
+    echo "run mlr failure"
+    exit -1
+fi
+
+#check the result
+ret=$(hdfs dfs -cat $hdfsoutput/evaluation | grep -Po "macroF1 : (.*)" |grep -Po "0\..*")
+echo "MacroF1="$ret
+eval=$(echo "($ret - 0.83 < 0) && ($ret -0.80 > 0)" | bc)
+if [ $eval -eq 1 ]; then
+    echo "Pass!"
+    exit 0
+else
+    echo "Fail!"
+    exit -1
+fi
+
+
---
+++ harp-tutorial-app/test_scripts/readme.md
@@ -0,0 +1,13 @@
+test scripts
+===============
+
+### kmeans.sh
+
+
+### mlr.sh
+
+Test on the [rcv1 dataset](http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/lyrl2004_rcv1v2_README.htm)
+with 103 classes, the macroF1 value on train set should be around 0.82.
+
+
+
