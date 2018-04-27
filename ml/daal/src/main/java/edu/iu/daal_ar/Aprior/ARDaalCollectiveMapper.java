1d286ac8db294d5efeabd32b2bdaf7dd118ef0bf
lc37 <lc37@indiana.edu>
2018-04-26 23:08:59 -0400

    add daal_ar

---
+++ ml/daal/src/main/java/edu/iu/daal_ar/Aprior/ARDaalCollectiveMapper.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2013-2016 Indiana University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
+
+ */
+
+package edu.iu.daal_ar.Aprior;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.DoubleBuffer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.CollectiveMapper;
+
+import edu.iu.harp.example.DoubleArrPlus;
+import edu.iu.harp.resource.DoubleArray;
+import edu.iu.harp.schdynamic.DynamicScheduler;
+import edu.iu.harp.example.IntArrPlus;
+
+import edu.iu.harp.partition.Partition;
+import edu.iu.harp.partition.PartitionStatus;
+import edu.iu.harp.partition.Partitioner;
+import edu.iu.harp.partition.Table;
+
+import edu.iu.harp.resource.DoubleArray;
+import edu.iu.harp.resource.IntArray;
+import edu.iu.harp.resource.ByteArray;
+import edu.iu.harp.resource.LongArray;
+
+import edu.iu.datasource.*;
+import edu.iu.data_aux.*;
+
+// import DAAL Java API
+import com.intel.daal.algorithms.classifier.prediction.ModelInputId;
+import com.intel.daal.algorithms.classifier.prediction.NumericTableInputId;
+import com.intel.daal.algorithms.classifier.prediction.PredictionResult;
+import com.intel.daal.algorithms.classifier.prediction.PredictionResultId;
+import com.intel.daal.algorithms.classifier.training.InputId;
+import com.intel.daal.algorithms.classifier.training.TrainingResultId;
+import com.intel.daal.algorithms.multi_class_classifier.Model;
+import com.intel.daal.algorithms.multi_class_classifier.prediction.*;
+import com.intel.daal.algorithms.multi_class_classifier.training.*;
+import com.intel.daal.data_management.data.NumericTable;
+import com.intel.daal.data_management.data.HomogenNumericTable;
+import com.intel.daal.data_management.data.MergedNumericTable;
+import com.intel.daal.data_management.data_source.DataSource;
+import com.intel.daal.data_management.data_source.FileDataSource;
+import com.intel.daal.services.DaalContext;
+import com.intel.daal.services.Environment;
+
+/**
+ * @brief the Harp mapper for running K-means
+ */
+public class ARDaalCollectiveMapper
+    extends
+    CollectiveMapper<String, String, Object, Object> {
+
+	//cmd args
+        private int numMappers;
+        private int numThreads;
+        private int harpThreads; 
+	private int fileDim;
+  	private String testFilePath;
+
+	private int nFeatures;
+    	private int nClasses;
+
+        //to measure the time
+        private long load_time = 0;
+        private long convert_time = 0;
+        private long total_time = 0;
+        private long compute_time = 0;
+        private long comm_time = 0;
+        private long ts_start = 0;
+        private long ts_end = 0;
+        private long ts1 = 0;
+        private long ts2 = 0;
+
+	private static HarpDAALDataSource datasource;
+
+	private static TrainingResult   trainingResult;
+	private static PredictionResult predictionResult;
+	private static NumericTable     testGroundTruth;
+
+	private static com.intel.daal.algorithms.svm.training.TrainingBatch twoClassTraining;
+	private static com.intel.daal.algorithms.svm.prediction.PredictionBatch twoClassPrediction;
+	private static DaalContext daal_Context = new DaalContext();
+
+        /**
+         * Mapper configuration.
+         */
+        @Override
+        protected void setup(Context context)
+        throws IOException, InterruptedException {
+
+        long startTime = System.currentTimeMillis();
+
+        Configuration configuration =
+            context.getConfiguration();
+
+	this.nFeatures = configuration.getInt(Constants.FEATURE_DIM, 10);
+	this.fileDim = configuration.getInt(Constants.FILE_DIM, 10);
+	this.nClasses = configuration.getInt(Constants.NUM_CLASS, 10);
+        this.numMappers = configuration.getInt(Constants.NUM_MAPPERS, 10);
+        this.numThreads = configuration.getInt(Constants.NUM_THREADS, 10);
+        //always use the maximum hardware threads to load in data and convert data 
+        this.harpThreads = Runtime.getRuntime().availableProcessors();
+	this.testFilePath = configuration.get(Constants.TEST_FILE_PATH,"");
+
+        LOG.info("File Dim " + this.fileDim);
+        LOG.info("Num Mappers " + this.numMappers);
+        LOG.info("Num Threads " + this.numThreads);
+        LOG.info("Num harp load data threads " + harpThreads);
+
+        long endTime = System.currentTimeMillis();
+        LOG.info("config (ms) :"
+                + (endTime - startTime));
+
+        }
+
+        // Assigns the reader to different nodes
+        protected void mapCollective(
+                KeyValReader reader, Context context)
+            throws IOException, InterruptedException {
+            long startTime = System.currentTimeMillis();
+
+	    // read data file names from HDFS
+            List<String> dataFiles =
+                new LinkedList<String>();
+            while (reader.nextKeyValue()) {
+                String key = reader.getCurrentKey();
+                String value = reader.getCurrentValue();
+                LOG.info("Key: " + key + ", Value: "
+                        + value);
+                LOG.info("file name: " + value);
+                dataFiles.add(value);
+            }
+            
+            Configuration conf = context.getConfiguration();
+
+	    // ----------------------- runtime settings -----------------------
+            //set thread number used in DAAL
+            LOG.info("The default value of thread numbers in DAAL: " + Environment.getNumberOfThreads());
+            Environment.setNumberOfThreads(numThreads);
+            LOG.info("The current value of thread numbers in DAAL: " + Environment.getNumberOfThreads());
+
+	    this.datasource = new HarpDAALDataSource(dataFiles, fileDim, harpThreads, conf);
+
+	    // ----------------------- start the execution -----------------------
+            runAR(conf, context);
+            this.freeMemory();
+            this.freeConn();
+            System.gc();
+        }
+
+        /**
+         * @brief run Association Rules by invoking DAAL Java API
+         *
+         * @param fileNames
+         * @param conf
+         * @param context
+         *
+         * @return 
+         */
+        private void runAR(Configuration conf, Context context) throws IOException 
+	{
+	    // ---------- load data ----------
+	    this.datasource.loadFiles();
+	    // ---------- training and testing ----------
+	    
+        }
+
+	
+
+}
