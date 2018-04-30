59f5dbd60b0698059bccc2d0952e477239a8732a
lc37 <lc37@indiana.edu>
2018-04-30 17:19:38 -0400

    add dforest classification to harp-daal

--- ml/daal/src/main/java/edu/iu/daal_dforest/ClsDenseBatch/DFCLSDaalCollectiveMapper.java
+++ ml/daal/src/main/java/edu/iu/daal_dforest/ClsDenseBatch/DFCLSDaalCollectiveMapper.java
@@ -16,7 +16,7 @@
 
  */
 
-package edu.iu.daal_dtree.ClsDenseBatch;
+package edu.iu.daal_dforest.ClsDenseBatch;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -56,16 +56,16 @@ import edu.iu.datasource.*;
 import edu.iu.data_aux.*;
 
 // intel daal algorithms 
-import com.intel.daal.algorithms.classifier.prediction.PredictionResult;
 import com.intel.daal.algorithms.classifier.prediction.ModelInputId;
 import com.intel.daal.algorithms.classifier.prediction.NumericTableInputId;
 import com.intel.daal.algorithms.classifier.training.InputId;
 import com.intel.daal.algorithms.classifier.training.TrainingResultId;
+import com.intel.daal.algorithms.classifier.prediction.PredictionResult;
 import com.intel.daal.algorithms.classifier.prediction.PredictionResultId;
-import com.intel.daal.algorithms.decision_tree.classification.Model;
-import com.intel.daal.algorithms.decision_tree.classification.prediction.*;
-import com.intel.daal.algorithms.decision_tree.classification.training.*;
-import com.intel.daal.algorithms.decision_tree.*;
+import com.intel.daal.algorithms.decision_forest.classification.Model;
+import com.intel.daal.algorithms.decision_forest.classification.prediction.*;
+import com.intel.daal.algorithms.decision_forest.classification.training.*;
+import com.intel.daal.algorithms.decision_forest.*;
 
 // intel daal data structures and services
 import com.intel.daal.data_management.data.NumericTable;
@@ -80,7 +80,7 @@ import com.intel.daal.data_management.data.*;
 /**
  * @brief the Harp mapper for running K-means
  */
-public class DTCLSDaalCollectiveMapper
+public class DFCLSDaalCollectiveMapper
     extends
     CollectiveMapper<String, String, Object, Object> {
 
@@ -90,10 +90,11 @@ public class DTCLSDaalCollectiveMapper
         private int harpThreads; 
 	private int fileDim;
   	private String testFilePath;
-  	private String pruneFilePath;
 
 	private int nFeatures;
     	private int nClasses;
+    	private int nTrees;
+    	private int minObservationsInLeafNode;
 
         //to measure the time
         private long load_time = 0;
@@ -123,15 +124,17 @@ public class DTCLSDaalCollectiveMapper
         Configuration configuration =
             context.getConfiguration();
 
-	this.nFeatures = configuration.getInt(Constants.FEATURE_DIM, 10);
-	this.fileDim = configuration.getInt(Constants.FILE_DIM, 10);
-	this.nClasses = configuration.getInt(Constants.NUM_CLASS, 10);
+	this.nFeatures = configuration.getInt(Constants.FEATURE_DIM, 3);
+	this.fileDim = configuration.getInt(Constants.FILE_DIM, 4);
+	this.nClasses = configuration.getInt(Constants.NUM_CLASS, 5);
+	this.nTrees = configuration.getInt(Constants.NUM_TREES, 10);
+	this.minObservationsInLeafNode = configuration.getInt(Constants.MIN_OBS_LEAFNODE, 8);
+
         this.numMappers = configuration.getInt(Constants.NUM_MAPPERS, 10);
         this.numThreads = configuration.getInt(Constants.NUM_THREADS, 10);
         //always use the maximum hardware threads to load in data and convert data 
         this.harpThreads = Runtime.getRuntime().availableProcessors();
 	this.testFilePath = configuration.get(Constants.TEST_FILE_PATH,"");
-	this.pruneFilePath = configuration.get(Constants.PRUNE_FILE_PATH,"");
 
         LOG.info("File Dim " + this.fileDim);
         LOG.info("Num Mappers " + this.numMappers);
@@ -173,7 +176,7 @@ public class DTCLSDaalCollectiveMapper
 	    this.datasource = new HarpDAALDataSource(dataFiles, fileDim, harpThreads, conf);
 
 	    // ----------------------- start the execution -----------------------
-            runDTCLS(conf, context);
+            runDFCLS(conf, context);
             this.freeMemory();
             this.freeConn();
             System.gc();
@@ -188,19 +191,19 @@ public class DTCLSDaalCollectiveMapper
          *
          * @return 
          */
-        private void runDTCLS(Configuration conf, Context context) throws IOException 
+        private void runDFCLS(Configuration conf, Context context) throws IOException 
 	{
 		// ---------- load data ----------
 		this.datasource.loadFiles();
 		// ---------- training and testing ----------
-
 		TrainingResult trainingResult = trainModel();
 		PredictionResult predictionResult = testModel(trainingResult);
 		printResults(predictionResult);
+
 		daal_Context.dispose();
 	}
 
-	private TrainingResult trainModel() throws IOException {
+	private TrainingResult trainModel() {
 
         /* Create Numeric Tables for training data and labels */
         NumericTable trainData = new HomogenNumericTable(daal_Context, Double.class, nFeatures, this.datasource.getTotalLines(), NumericTable.AllocationFlag.DoAllocate);
@@ -210,41 +213,37 @@ public class DTCLSDaalCollectiveMapper
         mergedData.addNumericTable(trainGroundTruth);
 
         /* Retrieve the data from an input file */
-        this.datasource.loadDataBlock(mergedData);
+	this.datasource.loadDataBlock(mergedData);
 
-	this.datasource.loadTestFile(pruneFilePath, fileDim);
+        /* Set feature as categorical */
+        DataFeature categoricalFeature = trainData.getDictionary().getFeature(2);
+        categoricalFeature.setFeatureType(DataFeatureUtils.FeatureType.DAAL_CATEGORICAL);
 
-        /* Create Numeric Tables for pruning data and labels */
-        NumericTable pruneData = new HomogenNumericTable(daal_Context, Double.class, nFeatures, this.datasource.getTestRows(), NumericTable.AllocationFlag.NotAllocate);
-        NumericTable pruneGroundTruth = new HomogenNumericTable(daal_Context, Double.class, 1, this.datasource.getTestRows(), NumericTable.AllocationFlag.NotAllocate);
-        MergedNumericTable pruneMergedData = new MergedNumericTable(daal_Context);
-        pruneMergedData.addNumericTable(pruneData);
-        pruneMergedData.addNumericTable(pruneGroundTruth);
-
-        /* Retrieve the pruning data from an input file */
-	this.datasource.loadTestTable(pruneMergedData);
-
-        /* Create algorithm objects to train the decision tree classification model */
+        /* Create algorithm objects to train the decision forest classification model */
         TrainingBatch algorithm = new TrainingBatch(daal_Context, Double.class, TrainingMethod.defaultDense, nClasses);
+        algorithm.parameter.setNTrees(nTrees);
+        algorithm.parameter.setFeaturesPerNode(nFeatures);
+        algorithm.parameter.setMinObservationsInLeafNode(minObservationsInLeafNode);
+        algorithm.parameter.setVariableImportanceMode(VariableImportanceModeId.MDI);
+        algorithm.parameter.setResultsToCompute(ResultsToComputeId.computeOutOfBagError);
 
-        /* Pass the training data set with labels, and pruning dataset with labels to the algorithm */
+        /* Pass a training data set and dependent values to the algorithm */
         algorithm.input.set(InputId.data, trainData);
         algorithm.input.set(InputId.labels, trainGroundTruth);
-        algorithm.input.set(TrainingInputId.dataForPruning, pruneData);
-        algorithm.input.set(TrainingInputId.labelsForPruning, pruneGroundTruth);
 
-        /* Train the decision tree classification model */
+        /* Train the decision forest classification model */
         TrainingResult trainingResult = algorithm.compute();
 
+        Service.printNumericTable("Variable importance results: ", trainingResult.get(ResultNumericTableId.variableImportance));
+        Service.printNumericTable("OOB error: ", trainingResult.get(ResultNumericTableId.outOfBagError));
         return trainingResult;
     }
 
 
-    private PredictionResult testModel(TrainingResult trainingResult) throws IOException
+    private PredictionResult testModel(TrainingResult trainingResult) throws IOException 
     {
 
 	this.datasource.loadTestFile(testFilePath, fileDim);
-
         /* Create Numeric Tables for testing data and labels */
         NumericTable testData = new HomogenNumericTable(daal_Context, Double.class, nFeatures, this.datasource.getTestRows(), NumericTable.AllocationFlag.DoAllocate);
         testGroundTruth = new HomogenNumericTable(daal_Context, Double.class, 1, this.datasource.getTestRows(), NumericTable.AllocationFlag.DoAllocate);
@@ -255,11 +254,14 @@ public class DTCLSDaalCollectiveMapper
         /* Retrieve the data from an input file */
 	this.datasource.loadTestTable(mergedData);
 
-        /* Create algorithm objects for decision tree classification prediction */
-        PredictionBatch algorithm = new PredictionBatch(daal_Context, Double.class, PredictionMethod.defaultDense);
+        /* Set feature as categorical */
+        DataFeature categoricalFeature = testData.getDictionary().getFeature(2);
+        categoricalFeature.setFeatureType(DataFeatureUtils.FeatureType.DAAL_CATEGORICAL);
+
+        /* Create algorithm objects for decision forest classification prediction with the fast method */
+        PredictionBatch algorithm = new PredictionBatch(daal_Context, Double.class, PredictionMethod.defaultDense, nClasses);
 
         /* Pass a testing data set and the trained model to the algorithm */
-        System.out.println("Number of columns: " + testData.getNumberOfColumns());
         Model model = trainingResult.get(TrainingResultId.model);
         algorithm.input.set(NumericTableInputId.data, testData);
         algorithm.input.set(ModelInputId.model, model);
@@ -267,10 +269,10 @@ public class DTCLSDaalCollectiveMapper
         /* Compute prediction results */
         return algorithm.compute();
     }
-
+ 
     private void printResults(PredictionResult predictionResult) {
         NumericTable predictionResults = predictionResult.get(PredictionResultId.prediction);
         Service.printClassificationResult(testGroundTruth, predictionResults, "Ground truth", "Classification results",
-                "Decision tree classification results (first 20 observations):", 20);
-    }
+                "Decision forest classification results (first 20 observations):", 20);
+    }  
 }
