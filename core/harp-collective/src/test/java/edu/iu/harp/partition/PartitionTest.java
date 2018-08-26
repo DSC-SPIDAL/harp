fd9731f611c66e996aca926d4be3e0a9708cd3f0
supunkamburugamuve <supun06@gmail.com>
2018-08-26 01:47:21 -0400

    adding partition test

--- core/harp-collective/src/test/java/edu/iu/harp/partition/PartitionTest.java
+++ core/harp-collective/src/test/java/edu/iu/harp/partition/PartitionTest.java
@@ -1,4 +1,49 @@
 package edu.iu.harp.partition;
 
+import edu.iu.harp.resource.IntArray;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PartitionTest {
+  @Test
+  public void testGet() {
+    int[] arr = new int[10];
+    IntArray array = new IntArray(arr, 0, 10);
+    Partition<IntArray> partition = new Partition<>(0, array);
+
+    Assert.assertNotNull(partition.get());
+    Assert.assertSame(partition.get(), array);
+    Assert.assertSame(partition.get().get(), arr);
+  }
+
+  @Test
+  public void testNumEncodedBytes() {
+    int[] arr = new int[10];
+    IntArray array = new IntArray(arr, 0, 10);
+    Partition<IntArray> partition = new Partition<>(0, array);
+
+    Assert.assertEquals(49, partition.getNumEnocdeBytes());
+  }
+
+  @Test
+  public void testId() {
+    int[] arr = new int[10];
+    IntArray array = new IntArray(arr, 0, 10);
+    Partition<IntArray> partition = new Partition<>(0, array);
+
+    Assert.assertEquals(0, partition.id());
+  }
+
+  @Test
+  public void testRelease() {
+    int[] arr = new int[10];
+    IntArray array = new IntArray(arr, 0, 10);
+    Partition<IntArray> partition = new Partition<>(0, array);
+
+    Assert.assertEquals(0, partition.id());
+
+    partition.release();
+    Assert.assertNull(partition.get());
+    Assert.assertNull(array.get());
+  }
 }
