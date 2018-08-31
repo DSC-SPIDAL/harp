6f59fb63c28900ede3a6280b040fc28b32a4348b
supunkamburugamuva <supun06@gmail.com>
2018-08-31 12:23:29 -0400

    fixing an error

--- core/harp-collective/src/test/java/edu/iu/harp/keyval/Long2DoubleKVPartitionTest.java
+++ core/harp-collective/src/test/java/edu/iu/harp/keyval/Long2DoubleKVPartitionTest.java
@@ -14,7 +14,7 @@ public class Long2DoubleKVPartitionTest {
     intKVPartition.putKeyVal(14, 10, new TypeDoubleCombiner());
     intKVPartition.putKeyVal(15, 10, new TypeDoubleCombiner());
 
-    Assert.assertEquals(10, intKVPartition.getVal(19));
+    Assert.assertEquals(10, intKVPartition.getVal(19), 0.0001);
   }
 
   @Test
@@ -23,7 +23,7 @@ public class Long2DoubleKVPartitionTest {
     intKVPartition.initialize();
     intKVPartition.putKeyVal(19, 10, new TypeDoubleCombiner());
 
-    Assert.assertEquals(10, intKVPartition.getVal(19));
+    Assert.assertEquals(10, intKVPartition.getVal(19), 0.001);
   }
 
   @Test
@@ -50,6 +50,6 @@ public class Long2DoubleKVPartitionTest {
     intKVPartition.putKeyVal(15, 10, new TypeDoubleCombiner());
 
     intKVPartition.clear();
-    Assert.assertEquals(Integer.MIN_VALUE, intKVPartition.getVal(19));
+    Assert.assertEquals(Double.NEGATIVE_INFINITY, intKVPartition.getVal(19), 0.001);
   }
 }
