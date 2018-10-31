a9d2ce059cab48e43ad616dabed500e56637249b
miajiang <miajiang@iu.edu>
2018-10-31 14:48:37 -0400

    Setup Skelecton of the code

--- sub/pGBRT-Miao_10.30/source/histogram.h
+++ sub/pGBRT-Miao_10.30/source/histogram.h
@@ -7,12 +7,18 @@
 
 using namespace std;
 
+/*
+ * The Bin structure contains the median value, the label and the number
+ */
 Struct Bin{
     double label;
     double value;
     int number;
 };
 
+/**
+ * The historgram collect the bins of feature data.
+ */
 class histogram{
 public:
     histogram(int numberofbins);      
@@ -20,14 +26,15 @@ public:
     void update_histogram();
     double getResidule();
     double setHistogram(int n, int i);
-    Bin mergeHistogram(Bin a,Bin b);
-    Bin addElement(Bin a,int element);
-   
+    Bin mergeHistogram(Bin* a,Bin* b);
+    Bin addElement(Bin* a,int element);
+    double    
 private:
     int numBin;
     vector<Bin> bins; 
-    double label;    
+    double* label;    
 };
+
 
 histogram::histogram(int numberofbins) {
     numBin=bin;
@@ -38,32 +45,39 @@ histogram::histogram(int numberofbins) {
 histogram::~histogram() {
     // delete all 1-d arrays: qid, label, node, pred, residual, idealdcg
 	
-	// delete all 2-d arrays: rawfeatures, sortedfeatures, sortedindices
-    for (int i=0; i<numBin; i++) {
-           bins.pop();
-           Bin[i] = NULL;
-        
+    // delete all 2-d arrays: rawfeatures, sortedfeatures, sortedindices
+    for (int i=0; i<numBin; i++){
+         Bin newbin=bins.pop();
+         newbin=NULL;
     }
+    
+    delete lable;
 }
 
 
 
 
-histogram::update_histogram(){
-    for (int i=0;i<numBin;i++){
-    	
-    }
-
+histogram::update_histogram(Bin* a,Bin* b){
+    int number= a->number+b->number;
+    
+    
 }
 
 
-histogram::merge_histogram(Bin a,Bin b){
-   
-   return NULL;
+histogram::merge_histogram(Bin* a,Bin* b){
+   int total_number=a->number+b->number;
+   double total=(a->value*a->number+b->value*b->number);
+   Bin new_bin=new Bin(a->label,total_number,total);
+   return new_bin;
 }
 
 histogram::addElement(Bin* a,int element){
-   double cur=a->   
+   double cur=a->value;   
 
-   return Null;
+   return NULL;
 }
+
+histogram::sum(Bin* a,Bin* b){
+
+   return NULL;
+} 
