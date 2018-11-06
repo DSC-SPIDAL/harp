3042ad248cd60809ba7f8505a6908a971ce81a2b
miajiang <miajiang@iu.edu>
2018-11-06 15:52:46 -0500

    Debug of histogram.

--- sub/pGBRT-Miao_10.30/source/histogram.h
+++ sub/pGBRT-Miao_10.30/source/histogram.h
@@ -1,16 +1,7 @@
 #ifndef HISTOGRAM_H
 #define HISTOGRAM_H
 
-#include <iostream>
-#include <vector>
-#include "feature_data.h"
-
-using namespace std;
-
-/*
- * The Bin structure contains the median value, the label and the number
- */
-Struct Bin{
+struct Bin{
     double label;
     double value;
     int number;
@@ -21,22 +12,38 @@ Struct Bin{
  */
 class histogram{
 public:
-    histogram(int numberofbins);      
+    histogram(int numberofbins);
     ~histogram();
+    Bin* merge(Bin* left,Bin* right);
 private:
     int numBin;
-    vector<Bin> bins; 
-    double* label;    
+    vector<Bin> bins;
+    double* label;
 };
 
 
 histogram::histogram(int numberofbins) {
-    numBin=bin;
-    bins=new Bin[numberofbins];
- 
+    numBin=numberofbins;
+    bins=vector<Bin>();
+    cout<<"build the bins"<<endl;
 }
 
+histogram::~histogram(){
 
+}
 
+Bin* histogram::merge(Bin* left, Bin* right){
+    int count1=left->number;
+    int count2=right->number;
+    double val1=left->value;
+    double val2=right->value;
+    Bin* result=new Bin();
+
+    result->number=count1+count2;
+    result->value=(val1+val2)/(result->number);
+    return result;
+}
+
+#endif
 
 
