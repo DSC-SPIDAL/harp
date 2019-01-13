807171f4dac8e05495ce7725dd82322f0826ee16
pengb <pengb@indiana.edu>
2019-01-12 23:25:23 -0500

    update evaluation toolset, run-speedup, validate, scaling

--- run/10.BlockParallelReport/test/run-speedup.sh
+++ run/10.BlockParallelReport/test/run-speedup.sh
@@ -1,61 +1,46 @@
+#!/bin/bash
 
-#../bin/xgb-speedup.sh ../bin/xgb-latest higgs 10 8 hist 32
-#../bin/xgb-speedup.sh ../bin/xgb-latest higgs 10 12 hist 32
-#../bin/xgb-speedup.sh ../bin/xgb-latest higgs 10 16 hist 32
-#
-#
-#bin=../bin/xgboost-g++-omp-halftrick-noprefetch-byte-blockdense
-#../bin/xgb-speedup.sh ${bin} higgs 10 8 blockdense 32
-#../bin/xgb-speedup.sh ${bin} higgs 10 12 blockdense 32
-#../bin/xgb-speedup.sh ${bin} higgs 10 16 blockdense 32
-#
-#../bin/xgb-speedup.sh ../bin/xgb-latest synset 10 8 hist 32
-#../bin/xgb-speedup.sh ../bin/xgb-latest synset 10 12 hist 32
-#../bin/xgb-speedup.sh ../bin/xgb-latest synset 10 16 hist 32
+if [ $# -eq "0"  ] ; then
+	echo "Usage: run-speedup.sh <bin>"
+	exit -1
+fi
 
-#bin=../bin/xgboost-g++-omp-nohalftrick-noprefetch-short-blockdense
-#../bin/xgb-speedup.sh ${bin} synset 10 8 blockdense 32 500000 16
-#../bin/xgb-speedup.sh ${bin} synset 10 12 blockdense 32 500000 16
-#../bin/xgb-speedup.sh ${bin} synset 10 16 blockdense 32 500000 16
+tagname=$1
+if [ -z $tagname  ] ; then
+	tagname=byte-blockdense-longcube-threadinit
+fi
 
-#bin=../bin/xgboost-g++-omp-halftrick-noprefetch-byte-blockdense
-#../bin/xgb-speedup.sh ${bin} higgs 10 8 blockdense 32
-#../bin/xgb-speedup.sh ${bin} higgs 10 12 blockdense 32
-#../bin/xgb-speedup.sh ${bin} higgs 10 16 blockdense 32
+echo "run speedup test with tagname=$tagname"
 
-#bin=../bin/xgboost-g++-omp-halftrick-noprefetch-short-blockdense-longcube
-#../bin/xgb-speedup.sh ${bin} synset 10 8 blockdense 32 500000 16
-#../bin/xgb-speedup.sh ${bin} synset 10 12 blockdense 32 500000 16
-#../bin/xgb-speedup.sh ${bin} synset 10 13 blockdense 32 500000 16
+bin=../bin/xgboost-g++-omp-halftrick-noprefetch-${tagname}-release
+if [ ! -f $bin ]; then
+	echo "$bin not exist, quit"
+	exit -1
+fi
+bin=../bin/xgboost-g++-omp-nohalftrick-noprefetch-${tagname}-release
+if [ ! -f $bin ]; then
+	echo "$bin not exist, quit"
+	exit -1
+fi
 
-
-#bin=../bin/xgboost-g++-omp-halftrick-noprefetch-byte-blockdense-longcube-release
-#../bin/xgb-speedup.sh ${bin} higgs 10 8 blockdense 32
-#../bin/xgb-speedup.sh ${bin} higgs 10 12 blockdense 32
-#../bin/xgb-speedup.sh ${bin} higgs 10 16 blockdense 32
-#
-#../bin/xgb-speedup.sh ${bin} synset 10 8 blockdense 32 
-#../bin/xgb-speedup.sh ${bin} synset 10 12 blockdense 32 
-#../bin/xgb-speedup.sh ${bin} synset 10 13 blockdense 32 
-
-bin=../bin/xgboost-g++-omp-halftrick-noprefetch-byte-blockdense-longcube-threadinit-release
+bin=../bin/xgboost-g++-omp-halftrick-noprefetch-${tagname}-release
 ../bin/xgb-speedup.sh ${bin} higgs 10 8 blockdense 32
 ../bin/xgb-speedup.sh ${bin} higgs 10 12 blockdense 32
 ../bin/xgb-speedup.sh ${bin} higgs 10 16 blockdense 32
 
 
-bin=../bin/xgboost-g++-omp-nohalftrick-noprefetch-byte-blockdense-longcube-threadinit-release
+bin=../bin/xgboost-g++-omp-nohalftrick-noprefetch-${tagname}-release
 ../bin/xgb-speedup.sh ${bin} higgs 10 8 blockdense 32
 ../bin/xgb-speedup.sh ${bin} higgs 10 12 blockdense 32
 ../bin/xgb-speedup.sh ${bin} higgs 10 16 blockdense 32
 
 
-bin=../bin/xgboost-g++-omp-halftrick-noprefetch-byte-blockdense-longcube-threadinit-release
+bin=../bin/xgboost-g++-omp-halftrick-noprefetch-${tagname}-release
 ../bin/xgb-speedup.sh ${bin} synset 10 8 blockdense 32 
 ../bin/xgb-speedup.sh ${bin} synset 10 12 blockdense 32 
 ../bin/xgb-speedup.sh ${bin} synset 10 13 blockdense 32 
 
-bin=../bin/xgboost-g++-omp-nohalftrick-noprefetch-byte-blockdense-longcube-threadinit-release
+bin=../bin/xgboost-g++-omp-nohalftrick-noprefetch-${tagname}-release
 ../bin/xgb-speedup.sh ${bin} synset 10 8 blockdense 32 
 ../bin/xgb-speedup.sh ${bin} synset 10 12 blockdense 32 
 ../bin/xgb-speedup.sh ${bin} synset 10 13 blockdense 32 
@@ -65,5 +50,5 @@ echo " Speedup Test Results:"
 echo "================================"
 # binname, runid, trainingtime
 echo -e "binname\trunid\ttrainingtime"
-find . -name "SpeedUp*threadinit*.csv" -exec cat {} \; |gawk -F, '{printf("%s\t%s\t%s\n",$1,$2,$5)}' |sort
+find . -name "SpeedUp*${tagname}*.csv" -exec cat {} \; |gawk -F, '{printf("%s\t%s\t%s\n",$1,$2,$5)}' |sort
 
