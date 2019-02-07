5ea4c977697ff449d415e1e103b40f75367e063e
Ahmet Uyar <ahmetuyar@gmail.com>
2019-02-07 11:14:14 +0300

    echo server added to test dns names of pods

--- core/harp-collective/src/main/java/edu/iu/harp/example/kmeans/k8s/HelloWorld.java
+++ core/harp-collective/src/main/java/edu/iu/harp/example/kmeans/k8s/HelloWorld.java
@@ -2,8 +2,14 @@ package edu.iu.harp.example.kmeans.k8s;
 
 import org.apache.log4j.Logger;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HelloWorld {
   protected static final Logger LOG = Logger.getLogger(HelloWorld.class);
+
+  public static int SERVER_PORT = 9009;
 
   public static void main(String[] args) {
     String podIP = System.getenv("POD_IP");
@@ -14,6 +20,8 @@ public class HelloWorld {
     LOG.info("My PodIP: " + podIP);
     LOG.info("My PodName: " + podName);
     LOG.info("numberOfWorkers: " + numberOfWorkers);
+
+    echoServer(SERVER_PORT, podName);
 
     waitIndefinitely();
   }
@@ -42,5 +50,54 @@ public class HelloWorld {
   public static int getPodIndex(String podName) {
     return Integer.parseInt(podName.substring(podName.lastIndexOf("-") + 1));
   }
+
+  /**
+   * an echo server.
+   */
+  public static void echoServer(int serverPort, String podName) {
+
+    // create socket
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(serverPort);
+    } catch (IOException e) {
+      LOG.warn("Could not start ServerSocket.", e);
+    }
+
+    LOG.info("Echo Server started on port " + serverPort);
+
+    // repeatedly wait for connections, and process
+    while (true) {
+
+      try {
+        // a "blocking" call which waits until a connection is requested
+        Socket clientSocket = serverSocket.accept();
+        LOG.info("Accepted a connection from the client:" + clientSocket.getInetAddress());
+
+        InputStream is = clientSocket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        out.println("hello from the server: " + podName);
+        out.println("Will echo your messages:");
+
+        String s;
+        while ((s = reader.readLine()) != null) {
+          out.println(s);
+        }
+
+        // close IO streams, then socket
+        LOG.info("Closing the connection with client");
+        out.close();
+        reader.close();
+        clientSocket.close();
+
+      } catch (IOException ioe) {
+        throw new IllegalArgumentException(ioe);
+      }
+    }
+  }
+
 
 }
