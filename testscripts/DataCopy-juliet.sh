989e24fc5ccade0574834c4e38bbce7b2f11ca47
Langshi-Chen <langshi.chen@hotmail.com>
2019-05-14 19:48:06 +0800

    add scripts for experiments

---
+++ testscripts/DataCopy-juliet.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+## copy data 
+## j-010 is the base node 
+tploc=/scratch_hdd/lc37/sc-vec/templates
+gloc=/scratch_hdd/lc37/sc-vec/graphs
+
+## failed connected nodes: j-090 j-091 j-070 j-099 has 18 cores
+
+#for ip in j-007 j-008 j-009 j-013 j-014 j-016 j-028 j-038 j-057 j-079 j-095 j-099
+for ip in j-008 j-013 j-014 j-016 j-028 j-038 j-057 j-079 j-095
+do
+
+echo "copy to ${ip}"
+
+### copy templates
+#ssh ${ip} "mkdir -p ${tploc}"
+#scp ${tploc}/* ${ip}:${tploc}/   
+
+## copy datasets
+ssh ${ip} "mkdir -p ${gloc}"
+for dataname in miami.graph
+do
+
+scp ${gloc}/${dataname} ${ip}:${gloc}/
+
+done
+done
--- testscripts/syncCluster.sh
+++ testscripts/syncCluster.sh
@@ -2,5 +2,7 @@
 
 runPath=/N/u/lc37/WorkSpace/PGBSCTest
 ssh $LCJULIET "mkdir -p ${runPath}"
-scp ./hsw-run-pgbsc-mpi.sh $LCJULIET:${runPath}/
+#scp ./hsw-run-pgbsc-mpi.sh $LCJULIET:${runPath}/
 scp ./machinehosts $LCJULIET:${runPath}/
+#scp ./check-nodes.sh $LCJULIET:${runPath}/
+scp ./DataCopy-juliet.sh $LCJULIET:${runPath}/
